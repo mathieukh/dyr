af576f5073eb14938f50ec37958a96ccf1801b2a
MathieuKh <mathieu.khalem@gmail.com>
2016-09-08 13:30:10 +0200

    + Ajout d'une banniere lors du display permettant de monétiser l'application et la possibilité de la désactiver dans les préférences

--- app/src/main/java/com/mathieukh/dyr/DisplayTasks/DisplayTasksActivity.java
+++ app/src/main/java/com/mathieukh/dyr/DisplayTasks/DisplayTasksActivity.java
@@ -1,12 +1,19 @@
 package com.mathieukh.dyr.DisplayTasks;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.common.base.Preconditions;
 import com.mathieukh.dyr.R;
+import com.mathieukh.dyr.Settings.SettingsFragment;
 import com.mathieukh.dyr.data.Injection;
 import com.mathieukh.dyr.utils.ActivityUtils;
 
@@ -17,12 +24,28 @@ public class DisplayTasksActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private DisplayTasksPresenter mDisplayTasksPresenter;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_act);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean displayAd = sharedPref.getBoolean(SettingsFragment.AD_PREF_KEY, true);
+        if(displayAd){
+            MobileAds.initialize(getApplicationContext(), "NOTDEFINED");
+        }
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mAdView = (AdView) findViewById(R.id.adView);
+
+        if(displayAd) {
+            AdRequest adRequest = new AdRequest.Builder().addTestDevice("NOT DEFINED").build();
+            mAdView.loadAd(adRequest);
+        }else {
+            mAdView.setVisibility(View.GONE);
+        }
+
         if (!getIntent().hasExtra("SSID") || !getIntent().hasExtra("ENTERING"))
             finish();
         String ssid = Preconditions.checkNotNull(getIntent().getStringExtra("SSID"));
@@ -41,6 +64,24 @@ public class DisplayTasksActivity extends AppCompatActivity {
         mDisplayTasksPresenter = new DisplayTasksPresenter(
                 Injection.provideTasksRepository(this),
                 tasksFragment, ssid, isEntering, savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdView.destroy();
     }
 
     @Override
--- app/src/main/java/com/mathieukh/dyr/Settings/SettingsFragment.java
+++ app/src/main/java/com/mathieukh/dyr/Settings/SettingsFragment.java
@@ -13,6 +13,7 @@ public class SettingsFragment extends PreferenceFragment {
     public static final String VIBRATOR_PREF_KEY = "pref_vibrator";
     public static final String SOUND_PREF_KEY = "pref_sound";
     public static final String LED_PREF_KEY = "pref_led";
+    public static final String AD_PREF_KEY = "pref_ad";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
